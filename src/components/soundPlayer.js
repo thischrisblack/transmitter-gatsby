0e239267f232dcb4b486d6d2f512b7ba9b34e256
Chris Black <black@chrisblack.net>
2020-01-04 21:10:21 -0700

    Styled sound player.

--- src/components/soundPlayer.js
+++ src/components/soundPlayer.js
@@ -1,6 +1,36 @@
 import React, { Component } from "react"
-
+import styled from "styled-components"
 import { formatTimer } from "../utils"
+
+const Player = styled.div`
+  font-size: 1.4rem;
+  padding: 0 0 0.5rem 0;
+`
+
+const Controls = styled.span`
+  display: inline-block;
+  width: 5rem;
+  font-weight: 700;
+  cursor: pointer;
+  padding: 0;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  -webkit-background-clip: text;
+  background-image: url(../img/static01.gif);
+  background-size: 12000%;
+`
+
+const Timer = styled.span`
+  color: #333;
+  padding: 0;
+`
+
+const ProgressBar = styled.span`
+  background-image: url(../img/static04.gif);
+  background-size: cover;
+  height: 1px;
+  display: block;
+`
 
 class SoundPlayer extends Component {
   state = {
@@ -49,7 +79,7 @@ class SoundPlayer extends Component {
 
   render() {
     return (
-      <div className="sound-player">
+      <Player>
         {this.state.trackLoadError && (
           <p className="error">
             At this time it seems I have surpassed my bandwidth quota for the
@@ -67,21 +97,20 @@ class SoundPlayer extends Component {
         >
           <p>Your browser doesn't support HTML5 audio.</p>
         </audio>
-        <span className="sound-player__controls" onClick={this.toggleAudio}>
+        <Controls onClick={this.toggleAudio}>
           {/* {this.state.playing ? "◼" : "▶"} */}
           {this.state.playing ? "STOP" : "PLAY"}
-        </span>
+        </Controls>
         {this.state.duration && (
-          <span className="sound-player__timer">
+          <Timer>
             {formatTimer(this.state.progress)} /{" "}
             {formatTimer(this.state.duration)}
-          </span>
+          </Timer>
         )}
-        <div
-          className="sound-player__progress-bar"
+        <ProgressBar
           style={{ width: this.state.progressPercent * 100 + "%" }}
         />
-      </div>
+      </Player>
     )
   }
 }
